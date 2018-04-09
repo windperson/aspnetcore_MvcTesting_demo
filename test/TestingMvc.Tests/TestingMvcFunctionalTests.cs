394b6dc283b2104e83a311cfd20bf1cdb8de2078
Chen Yu Pao <windperson@gmail.com>
2018-04-09 12:58:49 +0800

    Add project files.

---
+++ test/TestingMvc.Tests/TestingMvcFunctionalTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace TestingMvc.Tests
+{
+    public class TestingMvcFunctionalTests : IClassFixture<TestingMvcTestFixture<Startup>>
+    {
+        public TestingMvcFunctionalTests(TestingMvcTestFixture<Startup> fixture)
+        {
+            Client = fixture.CreateClient(new Uri("https://localhost:44389"));
+        }
+
+        public HttpClient Client { get; set; }
+
+        [Fact]
+        public async Task GetHomePage()
+        {
+            //Act
+            var response = await Client.GetAsync("/");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task GetNotExistWillGet404()
+        {
+            //Act
+            var response = await Client.GetAsync("/not-exist");
+
+            //Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
---
+++ test/TestingMvc.Tests/TestingMvcTestFixture.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace TestingMvc.Tests
+{
+    public class TestingMvcTestFixture<TStartup> : WebApplicationTestFixture<TStartup> where TStartup : class
+    {
+        public TestingMvcTestFixture() : base("src/TestingMvc")
+        { }
+    }
+}
